f335e45b83420c4d91a611d9de6f87890bbc72a4
Oluwamayowa <ademayo234@gmail.com>
2026-05-27 17:06:06 -0700

    removed cors specification for the frontend

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors({
-   origin: "https://reekol.vercel.app"
-}))
+app.use(cors())
 
 app.use(express.json())
 app.use('/user', userRoute)
